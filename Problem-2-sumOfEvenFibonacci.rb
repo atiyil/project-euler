8ce91b185914fd3ad95f03644a18e5865888c5c4
atiyil <atiyil@gmail.com>
2018-03-05 03:30:46 +0000

    initial

---
+++ Problem-2-sumOfEvenFibonacci.rb
@@ -0,0 +1,21 @@
+# Problem-2: By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+# find the sum of the even-valued terms.
+
+def sumOfEvenFibonacci(n)
+	# sum of f(2),f(5),f(8),f(11),f(14),etc is needed. f(5) is 8, f(6) is 13, f(7) is 21
+	sum = 10
+	fn2 = 8
+	fn1 = 13
+	fn = 21
+	i = 8
+	while (fn < n)
+		fn2 = fn1
+		fn1 = fn
+		fn = fn1 + fn2
+		i += 1
+		if i % 3 == 0 then sum = sum + fn end
+	end
+	return sum
+end
+
+puts sumOfEvenFibonacci(4000000)
